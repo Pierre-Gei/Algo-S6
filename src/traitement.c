e0148d48d09e4bed8c55869b09650a59c2ea3034
Pierre-Gei <pierregeiguer@gmail.com>
2024-03-15 09:39:28 +0100

    Update doc

--- src/traitement.c
+++ src/traitement.c
@@ -9,6 +9,10 @@
 #include "stdlib.h"
 #include "../include/structures.h"
 
+/* `add_regle` est une fonction qui ajoute une nouvelle règle à une liste de règles. Il prend
+l'adresse de la tête de liste, le nom de la règle et une liste de conditions associées à la
+règle. Si la liste est vide, il crée un nouveau nœud pour la règle et le place au début de la
+liste. Si la liste n'est pas vide, il ajoute récursivement la règle à la fin de la liste. */
 void add_regle(liste_regles **liste, char *name, liste_conditions *conditions)
 {
     if (*liste == NULL) {
@@ -35,7 +39,9 @@ void add_regle(liste_regles **liste, char *name, liste_conditions *conditions)
 }
 
 
-
+/* La fonction `add_condition` est chargée d'ajouter une nouvelle condition à une liste de conditions.
+Il faut un pointeur vers la tête de la liste (« liste ») et le nom de la condition à ajouter (« nom
+»). */
 void add_condition(liste_conditions **liste, char *name)
 {
     if (*liste == NULL) {
@@ -61,6 +67,9 @@ void add_condition(liste_conditions **liste, char *name)
 
 
 
+/* La fonction `add_condition_to_regle` est chargée d'ajouter une nouvelle condition à une règle
+spécifique dans une liste de règles. Il prend l'adresse du tête de liste des règles, le nom de la
+règle à laquelle la condition doit être ajoutée et le nom de la condition à ajouter. */
 void add_condition_to_regle(liste_regles **liste, char *name_regle, char *name_condition)
 {
     if (*liste == NULL) {
@@ -79,6 +88,11 @@ void add_condition_to_regle(liste_regles **liste, char *name_regle, char *name_c
 
 
 
+/* La fonction `print_liste_regles` se charge d'imprimer la liste des règles de manière récursive. Cela
+commence par vérifier si la liste est vide. Si la liste n'est pas vide, elle parcourt chaque règle
+de la liste. Pour chaque règle, il imprime les conditions associées puis le nom de la règle
+elle-même. Ce processus se poursuit de manière récursive jusqu'à ce que toutes les règles de la
+liste aient été imprimées. */
 void print_liste_regles(liste_regles *liste) /////////// RENDRE RECURSIF
 {
     printf("Printing list of rules:\n");
@@ -111,7 +125,10 @@ void print_liste_regles(liste_regles *liste) /////////// RENDRE RECURSIF
     }
 }
 
-void add_fait(liste_faits **liste, char *fait) {
+/* La fonction `add_fait` est chargée d'ajouter un nouveau fait à une liste de faits. Il faut un
+pointeur vers la tête de liste (« liste ») et le nom du fait à ajouter (« fait »). */
+void add_fait(liste_faits **liste, char *fait)
+{
     if (*liste == NULL) {
         liste_faits *new = malloc(sizeof(liste_faits));
         if (new == NULL) {
@@ -131,6 +148,8 @@ void add_fait(liste_faits **liste, char *fait) {
     }
 }
 
+/* La fonction `affiche_liste_faits` se charge d'afficher la liste des faits. Il prend en paramètre un
+pointeur vers la tête de la liste de faits (`liste`). */
 void affiche_liste_faits(liste_faits *liste)
 {
     if (liste == NULL)
@@ -148,6 +167,9 @@ void affiche_liste_faits(liste_faits *liste)
 
 
 
+/* La fonction `liberer_liste_reponses` se charge de libérer la mémoire allouée pour une liste chaînée
+de réponses. Il prend un pointeur vers la tête de la liste en tant que paramètre et parcourt la
+liste de manière récursive pour libérer chaque nœud. */
 void  liberer_liste_reponses(liste_reponses **liste)
 {
     if (*liste == NULL)
@@ -158,6 +180,9 @@ void  liberer_liste_reponses(liste_reponses **liste)
     *liste = NULL;
 }
 
+/* La fonction `liberer_liste_faits(liste_faits **liste)` se charge de libérer la mémoire allouée pour
+une liste chaînée de faits. Il parcourt la liste de manière récursive et libère chaque nœud ainsi
+que sa mémoire associée.*/
 void liberer_liste_faits(liste_faits **liste)
 {
     if (*liste == NULL)
@@ -167,8 +192,13 @@ void liberer_liste_faits(liste_faits **liste)
     free(*liste);
     *liste = NULL;
 }
-void liberer_liste_conditions(liste_conditions **liste) {
-    printf("liberer_liste_conditions\n");
+
+
+/* La fonction `liberer_liste_conditions(liste_conditions **liste)` se charge de libérer la mémoire
+ * allouée pour une liste chaînée de conditions. Elle parcourt la liste de manière récursive et
+ * libère chaque nœud ainsi que sa mémoire associée.*/
+void liberer_liste_conditions(liste_conditions **liste)
+{
     if (*liste == NULL)
         return;
 
@@ -183,7 +213,9 @@ void liberer_liste_conditions(liste_conditions **liste) {
     *liste = suivant;
 }
 
-
+/* La fonction `liberer_liste_regles(liste_regles **liste)` se charge de libérer la mémoire allouée pour
+ * une liste chaînée de règles. Elle parcourt la liste de manière récursive et libère chaque nœud
+ * ainsi que sa mémoire associée.*/
 void liberer_liste_regles(liste_regles **liste)
 {
     if (*liste == NULL)
@@ -195,6 +227,10 @@ void liberer_liste_regles(liste_regles **liste)
     *liste = NULL;
 }
 
+/* La fonction `affiche_faits_possibles` est chargée d'afficher les faits possibles en fonction de la
+ * base de faits. Elle prend une liste de règles en paramètre et parcourt chaque règle pour extraire
+ * les faits possibles. Elle utilise une liste de réponses pour éviter d'afficher plusieurs fois le
+ * même fait. */
 void affiche_faits_possibles(liste_regles *liste) {
     liste_reponses *liste_reponses = NULL;
     printf("Liste des faits possibles d'après la base de faits : \n");
@@ -211,7 +247,6 @@ void affiche_faits_possibles(liste_regles *liste) {
             conditions = conditions->suivant;
         }
 
-        // Check the rule's conclusion
         if (liste->name != NULL) {
             if (!reponse_deja_donnee(liste_reponses, liste->name)) {
                 printf("  -%s\n", liste->name);
@@ -225,6 +260,9 @@ void affiche_faits_possibles(liste_regles *liste) {
     liberer_liste_reponses(&liste_reponses);
 }
 
+/* La fonction `reponse_deja_donnee` vérifie si une réponse a déjà été donnée. Elle prend une liste de
+ * réponses et une réponse en paramètre et parcourt la liste pour vérifier si la réponse est déjà
+ * présente. */
 bool reponse_deja_donnee(liste_reponses *liste, char *reponse) {
     while (liste != NULL) {
         if (strcmp(liste->name, reponse) == 0) {
@@ -235,6 +273,8 @@ bool reponse_deja_donnee(liste_reponses *liste, char *reponse) {
     return false;
 }
 
+/* La fonction `ajouter_reponse` est chargée d'ajouter une réponse à une liste de réponses. Elle prend
+ * un pointeur vers la tête de la liste de réponses et une réponse en paramètre. */
 void ajouter_reponse(liste_reponses **liste, char *reponse) {
     liste_reponses *nouvelle_reponse = malloc(sizeof(liste_reponses));
     nouvelle_reponse->name = strdup(reponse);
